fd63cdccfcc3790e70f84b7f1337318dd4099bb1
ashwin <ashwin.selvendran@cognizant.com>
2022-03-03 10:47:19 -0500

    Recycler view completed. Adding add data functionality

--- app/src/main/java/android/example/studentproject/MainActivity.java
+++ app/src/main/java/android/example/studentproject/MainActivity.java
@@ -3,12 +3,26 @@ package android.example.studentproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    View addData;
+    String ETname;
+    String ETnumber;
+    private ProjectViewModel projectViewModel;
+    private Student student;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addData(View view) {
+        ETname = findViewById(R.id.ETname).toString();
+        ETnumber = findViewById(R.id.ETnumber).toString();
+        student.name = ETname;
+        student.ph = ETnumber;
+        projectViewModel.insertProject(student);
+        finish();
     }
 }
--- app/src/main/java/android/example/studentproject/StudentAdapter.java
+++ app/src/main/java/android/example/studentproject/StudentAdapter.java
@@ -1,5 +1,58 @@
 package android.example.studentproject;
 
-public class StudentAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.ViewHolder> {
+
+    List<Student> localData;
+    private ProjectViewModel viewModel;
+
+    public StudentAdapter() {
+        localData = viewModel.getAllProject();
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.wordlist_item,
+                parent, false);
+        return new ViewHolder(view, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Student mCurrent = localData.get(position);
+        ViewHolder.nameView.setText(mCurrent.name);
+        ViewHolder.numView.setText(mCurrent.ph);
+    }
+
+    @Override
+    public int getItemCount() {
+        if(localData!=null){
+        return localData.size();}
+        else
+            return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        static TextView nameView;
+        static TextView numView;
+        final StudentAdapter mAdapter;
+
+        public ViewHolder(View view ,StudentAdapter mAdapter ) {
+            super(view);
+            nameView = view.findViewById(R.id.textName);
+            numView = view.findViewById(R.id.textNumber);
+            this.mAdapter = mAdapter;
+        }
+    }
 }
